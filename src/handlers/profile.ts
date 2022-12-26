0ad2b5639383d00b32c2ba37ac8da4c62567114e
vabgalimov <vabgalimov@vivaldi.net>
2022-12-26 15:29:58 +0500

    Add profile handler

--- src/handlers/profile.ts
+++ src/handlers/profile.ts
@@ -1,8 +1,29 @@
 import { comp } from "./composer"
 import { command } from "filters"
 
-comp.filter(command("profile.command"), ctx => {
+comp.filter(command("profile.command"), async ctx => {
     ctx.autoQuote()
 
+    const { user } = ctx.session
+    const { profile } = user
+    const text = ctx.t(`profile.templates.${profile.template}`, {
+        name: user.name,
+        status: 'TODO',
+        rank: 'TODO',
+        messages: profile.messages,
+        messagesToRankUp: 'TODO',
+        coins: profile.coins,
+        rating: profile.rating,
+        petName: 'TODO',
+        petSize: 'TODO',
+        petPower: 'TODO',
+        petBattleWins: 'TODO',
+        petBattleTotal: 'TODO',
+        duelWins: profile.duelWins,
+        duelTotal: profile.duelTotal,
+        firstChat: user.chats[0].name
+    })
+
     console.log(ctx.session)
+    await ctx.reply(text)
 })
