39d6f0b16d6bc38b8ddcf5113cca8e294f959781
vabgalimov <vabgalimov@vivaldi.net>
2022-12-29 15:41:58 +0500

    Add ctx.session.reply_to_session

--- src/middlewares/session.ts
+++ src/middlewares/session.ts
@@ -1,13 +1,22 @@
 import { comp } from "./composer"
 import { Chat } from "database/entity/chat"
 import { User } from "database/entity/user"
+import { SessionData } from "types/session"
 import { Chat as TelegramChat, User as TelegramUser } from "grammy/out/types"
 
 comp.use(async (ctx, next) => {
     const chat = await getChat(ctx.chat.id)
     const user = await getUser(ctx.from.id)
+    let reply_to_session: SessionData | null = null
+    if (ctx.msg?.reply_to_message?.from) {
+        const { chat: rTgChat, from: rTgUser } = ctx.msg.reply_to_message
+        const rChat = await getChat(ctx.msg.reply_to_message.chat.id)
+        const rUser = await getUser(ctx.msg.reply_to_message.from.id)
+        reply_to_session = await saveChatUser(rChat, rUser, rTgChat, rTgUser)
+    }
     ctx.session = {
         ...await saveChatUser(chat, user, ctx.chat, ctx.from),
+        reply_to_session: reply_to_session ?? ctx.session,
         async get(user_id, chat_id) {
             const user = await getUser(user_id)
             if (!user)
@@ -23,7 +32,7 @@ comp.use(async (ctx, next) => {
     await next()
 })
 
-async function saveChatUser(chat: Chat | null, user: User | null, tgChat: TelegramChat, tgUser: TelegramUser) {
+async function saveChatUser(chat: Chat | null, user: User | null, tgChat: TelegramChat, tgUser: TelegramUser): Promise<SessionData> {
     const upChat = new Chat(tgChat)
     if (chat) {
         chat.type = upChat.type
--- src/types/context/session.ts
+++ src/types/context/session.ts
@@ -3,6 +3,9 @@ import { SessionData } from "../session"
 export interface SessionFlavor {
     session: SessionData & {
         get(user_id: number, chat_id?: number): Promise<SessionData | null>
+
+        /** If not reply, then alias for ctx.session */
+        reply_to_session: SessionData
     }
 
     /** Alias for ctx.session.user.profile */
