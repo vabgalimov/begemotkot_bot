a852349213ac62d2ba7deb5bb8f2eb7a83d961f7
vabgalimov <vabgalimov@vivaldi.net>
2022-12-29 15:19:29 +0500

    Add on bot added and /start, /help handlers

--- src/handlers/help.ts
+++ src/handlers/help.ts
@@ -1,6 +1,19 @@
 import { comp } from "./composer"
-import { command } from "filters"
+import { botWasAdded, command } from "filters"
 import { helpMenu } from "utils/help-menu"
+
+comp.filter(botWasAdded(), async ctx => {
+    const text = helpMenu.ru
+    await ctx.reply(text)
+})
+
+comp.command(["start", "help"], async ctx => {
+    ctx.autoQuote()
+
+    const text = helpMenu[ctx.session.user.language_code]
+    await ctx.reply(text)
+})
+
 
 comp.filter(command("help.command"), async ctx => {
     ctx.autoQuote()
